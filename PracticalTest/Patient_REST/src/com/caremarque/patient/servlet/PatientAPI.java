46ab57cf15c65c0944e3477d0737b6180138ad87
KaushalyaMadushani <mkaushalya96@gmail.com>
2020-05-03 18:00:29 +0530

    Implement a custom method to  reads the request parameters, store them in a Map

--- PracticalTest/Patient_REST/src/com/caremarque/patient/servlet/PatientAPI.java
+++ PracticalTest/Patient_REST/src/com/caremarque/patient/servlet/PatientAPI.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.caremarque.patient.model.Patient;
 import com.caremarque.patient.service.PatientServiceImpl;
 
@@ -67,6 +65,30 @@ public class PatientAPI extends HttpServlet {
 		
 	}
 	
-
+	private static Map getParasMap(HttpServletRequest request) {
+			
+			Map<String, String> map = new HashMap<String, String>();
+			
+			try {
+				
+				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+				String queryString = scanner.hasNext() ?
+									 scanner.useDelimiter("\\A").next() : "";
+				
+				scanner.close();
+				
+				String[] params = queryString.split("&");
+				for(String param : params) {
+					
+					String[] p = param.split("=");
+					map.put(p[0], p[1]);
+				}
+									 
+			}
+			catch (Exception e) {
+				
+			}
+			return map;
+		}
 
 }
